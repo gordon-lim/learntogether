0c912391127745e5e4df6af540a8751aea491544
bongbonglemon <gordonlim214@gmail.com>
2021-07-08 02:04:16 +0800

    moved header out of hero component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import AnimationRevealPage from "helpers/AnimationRevealPage";
 import Hero from "components/hero/TwoColumnWithVideo";
 import Footer from "components/footers/MiniCenteredFooter";
 import Cards from "components/cards/ThreeColSlider";
+import Header from "components/headers/light.js";
 
 function App() {
   return (
     <AnimationRevealPage>
+      <Header />
       <Hero />
       <Cards />
       <Footer />
