ba9580d97f1158c1eab466cb9d9df5f225fc766b
MarcusPang <marcuspangyuyang@gmail.com>
2021-07-18 00:19:35 +0800

    Fixed carousel issue

--- app/components/Carousel/index.js
+++ app/components/Carousel/index.js
@@ -10,27 +10,27 @@ import PropTypes from 'prop-types';
 import { v4 } from 'uuid';
 // import styled from 'styled-components';
 
+const sliderSettings = {
+  arrows: false,
+  infinite: true,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+};
 function Carousel({ CardComponent, details }) {
-  const sliderSettings = {
-    arrows: false,
-    slidesToShow: 4,
-    responsive: [
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-
-      {
-        breakpoint: 900,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <Slider {...sliderSettings}>
       {details.map(item => (
--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,33 +6,38 @@
  */
 
 import { Box, Container } from '@chakra-ui/react';
-import React from 'react';
+import CourseCard from 'components/Card/CourseCard';
 import Carousel from 'components/Carousel';
 import Hero from 'components/Hero';
-import CourseCard from 'components/Card/CourseCard';
+import React from 'react';
+import { useStore } from 'react-redux';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+
+const details = [
+  {
+    category: 'Web Development',
+    title: 'React Course',
+    duration: '100 Hours',
+  },
+  {
+    category: 'Web Development',
+    title: 'Node Course',
+    duration: '100 Hours',
+  },
+  {
+    category: 'Web Development',
+    title: 'Django Course',
+    duration: '100 Hours',
+  },
+];
 
 export default function HomePage() {
-  const details = [
-    {
-      category: 'Web Development',
-      title: 'React Course',
-      duration: '100 Hours',
-    },
-    {
-      category: 'Web Development',
-      title: 'Node Course',
-      duration: '100 Hours',
-    },
-    {
-      category: 'Web Development',
-      title: 'Django Course',
-      duration: '100 Hours',
-    },
-  ];
-
+  const store = useStore();
   return (
     <Box>
       <Hero />
+      <div>{store.getState()}</div>
       {/* <UpcomingCarousel /> */}
       <Container maxW="7xl" py={12}>
         <Carousel CardComponent={CourseCard} details={details} />
