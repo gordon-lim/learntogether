7618dbd537936cf9066f1908ece15564a3974a6e
bongbonglemon <gordonlim214@gmail.com>
2021-07-19 22:15:29 +0800

    :sparkles: Completed course card landing/profile

--- app/components/Card/CourseCard.js
+++ app/components/Card/CourseCard.js
@@ -1,81 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Box,
-  Center,
-  useColorModeValue,
-  Heading,
-  Text,
-  Stack,
-  Image,
-} from '@chakra-ui/react';
+import { Box, Image, Badge } from '@chakra-ui/react';
 
-const IMAGE =
-  'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
-
-export default function CourseCard({ data }) {
+function CourseCard({ data }) {
   return (
-    <Center py={12}>
-      <Box
-        role="group"
-        p={6}
-        maxW="330px"
-        w="full"
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow="2xl"
-        rounded="lg"
-        pos="relative"
-        zIndex={1}
-      >
-        <Box
-          rounded="lg"
-          mt={-12}
-          pos="relative"
-          height="230px"
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${IMAGE})`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}
-        >
-          <Image
-            rounded="lg"
-            height={230}
-            width={282}
-            objectFit="cover"
-            src={IMAGE}
-          />
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Image src={data.imageUrl} alt={data.imageAlt} />
+
+      <Box p="6">
+        <Box d="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="green">
+            Ongoing
+          </Badge>
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+            {data.startDate} - {data.endDate}
+          </Box>
         </Box>
-        <Stack pt={10} align="center">
-          <Text color="gray.500" fontSize="sm" textTransform="uppercase">
-            {data.category}
-          </Text>
-          <Heading fontSize="2xl" fontFamily="body" fontWeight={500}>
-            {data.title}
-          </Heading>
-          <Stack direction="row" align="center">
-            <Text fontWeight={800} fontSize="xl">
-              {data.duration}
-            </Text>
-          </Stack>
-        </Stack>
+
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          isTruncated
+        >
+          {data.title}
+        </Box>
+        <Box
+          color="gray.500"
+          fontWeight="semibold"
+          letterSpacing="wide"
+          fontSize="xs"
+          textTransform="uppercase"
+        >
+          {data.timing}
+        </Box>
       </Box>
-    </Center>
+    </Box>
   );
 }
 
 CourseCard.propTypes = {
   data: PropTypes.object,
 };
+
+export default CourseCard;
