0c2d62ad26cef21f24d88e99688937be51761d76
mazx4960 <desmondtan4561@gmail.com>
2021-07-21 18:02:21 +0000

    Deploying to gh-pages from @ mazx4960/learntogether@809b0523ffdce46334d00b0758e5ebdbe8c65178 🚀

--- sw.js
+++ sw.js
@@ -67,7 +67,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "7/21/2021, 5:16:02 PM",
+  "version": "7/21/2021, 6:02:17 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
