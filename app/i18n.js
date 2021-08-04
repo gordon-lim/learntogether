297230e04d7681f28f64b2f7dab13d7640d654a7
Desmond <desmondtan4561@gmail.com>
2021-08-04 13:41:47 +0800

    :bug: fixed build issues

--- app/i18n.js
+++ app/i18n.js
@@ -7,21 +7,19 @@
  *   script `extract-intl`, and must use CommonJS module syntax
  *   You CANNOT use import/export in this file.
  */
-const addLocaleData = require('react-intl').addLocaleData; //eslint-disable-line
-const enLocaleData = require('react-intl/locale-data/en');
+import '@formatjs/intl-pluralrules/polyfill';
+import '@formatjs/intl-pluralrules/locale-data/en'; // locale-data for en
 
 const enTranslationMessages = require('./translations/en.json');
 
-addLocaleData(enLocaleData);
-
-const DEFAULT_LOCALE = 'en';
+export const DEFAULT_LOCALE = 'en';
 
 // prettier-ignore
-const appLocales = [
+export const appLocales = [
   'en',
 ];
 
-const formatTranslationMessages = (locale, messages) => {
+export const formatTranslationMessages = (locale, messages) => {
   const defaultFormattedMessages =
     locale !== DEFAULT_LOCALE
       ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
@@ -36,11 +34,6 @@ const formatTranslationMessages = (locale, messages) => {
   return Object.keys(messages).reduce(flattenFormattedMessages, {});
 };
 
-const translationMessages = {
+export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
 };
-
-exports.appLocales = appLocales;
-exports.formatTranslationMessages = formatTranslationMessages;
-exports.translationMessages = translationMessages;
-exports.DEFAULT_LOCALE = DEFAULT_LOCALE;
